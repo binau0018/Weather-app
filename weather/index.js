d5c3938795b134171c046bff92a4b59e02413175
Vinay <127412984+binau0018@users.noreply.github.com>
2025-08-02 20:14:51 +0530

    Update index.js

--- weather/index.js
+++ weather/index.js
@@ -1,5 +1,6 @@
-const Api = "9fa8999e4e80625dc298a7a03924d0ff";
-const apiurl = "YOUR_API_KEY_HERE"
+const Api = "your_api_key";
+
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="
 
 const searchBar = document.querySelector("#searchBar input");
 const searchBtn = document.querySelector("#searchBar button");
@@ -38,4 +39,5 @@ async function checkWeather(city) {
 searchBtn.addEventListener("click",() =>{
 	checkWeather(searchBar.value);
 })
+
 
